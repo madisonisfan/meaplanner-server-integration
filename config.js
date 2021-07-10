3ec987abe37bf8f1bf0af90c74558f5b4a597751
Madison Isfan <isfanmadison@gmail.com>
2021-07-10 11:14:22 -0700

    1st commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/mealplanner",
+  facebook: {
+    clientId: "433721084847859",
+    clientSecret: "6b880cb617a86bec659ab5696cd2aff8",
+  },
+};
