155fe8a861f75c7b4e0e6fe590d3cf87510c45b6
Madison Isfan <isfanmadison@gmail.com>
2021-07-10 14:40:44 -0700

    GET posts & recipes

---
+++ .history/app_20210710130943.js
@@ -0,0 +1,83 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+
+var logger = require("morgan");
+const passport = require("passport");
+const authenticate = require("./authenticate");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const recipeRouter = require("./routes/recipeRouter");
+const postRouter = require("./routes/postRouter");
+const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
+
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/mealplanner";
+
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  () => console.log("Connectly correctly to server"),
+  (err) => console.log(err)
+);
+
+var app = express();
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
+
+app.use(passport.initialize());
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/recipes", recipeRouter);
+app.use("/posts", postRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorite", favoriteRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ .history/routes/postRouter_20210710140830.js
@@ -0,0 +1,326 @@
+const express = require("express");
+const postRouter = express.Router();
+const Post = require("../models/postModel");
+const authenticate = require("../authenticate");
+const cors = require("./cors");
+
+postRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Post.find()
+      //.populate("postCreator")
+      .then((posts) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(posts);
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    req.body.postCreator = req.user._id;
+    Post.create(req.body)
+      .then((post) => {
+        console.log("Post created: ", post);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(post);
+      })
+      .catch((err) => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("PUT operation not support on /posts");
+  })
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Post.deleteMany()
+        .then((response) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.json(response);
+        })
+        .catch((err) => next(err));
+    }
+  );
+
+postRouter
+  .route("/:postId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(post);
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`POST operation not support on /posts/${req.params.postId}`);
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        if (post) {
+          if (post.postCreator._id.equals(req.user._id)) {
+            if (req.body.postContent) {
+              post.postContent = req.body.postContent;
+            }
+            if (req.body.postType) {
+              post.postType = req.body.postType;
+            }
+
+            post
+              .save()
+              .then((post) => {
+                res.statusCode = 200;
+                res.setHeader("Conten-Type", "application/json");
+                res.json(post);
+              })
+              .catch((err) => next(err));
+          } else {
+            const err = new Error("You are not the post creator");
+            err.status = 404;
+            return next(err);
+          }
+        } else {
+          const err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        if (post) {
+          if (post.postCreator._id.equals(req.user._id)) {
+            Post.findByIdAndDelete(req.params.postId)
+              .then((response) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(response);
+              })
+              .catch((err) => next(err));
+          } else {
+            const err = new Error(`You are not the post creator`);
+            err.status = 404;
+            return next(err);
+          }
+        } else {
+          const err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+/*
+  Post.findById(req.params.postId)
+      .then((post) => {
+        if (post) {
+          if (post.postCreator._id.equals(req.user._id)) {
+            post.remove();
+          } else {
+            const err = new Error(`You are not the post creator`);
+            err.status = 404;
+            return next(err);
+          }
+        } else {
+          const err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+
+  */
+
+postRouter
+  .route("/:postId/comments")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .populate("comments.commentCreator")
+      .then((post) => {
+        if (post) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(post.comments);
+        } else {
+          err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        if (post) {
+          req.body.commentCreator = req.user._id;
+          post.comments.push(req.body);
+          post
+            .save()
+            .then((post) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(post);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `PUT operation not supported on /posts/${req.params.postId}/comments`
+    );
+  })
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Post.findById(req.params.postId)
+        .then((post) => {
+          if (post) {
+            for (let i = post.comments.length - 1; i >= 0; i--) {
+              post.comments.id(post.comments[i]._id).remove();
+            }
+            post
+              .save()
+              .then((post) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(post);
+              })
+              .catch((err) => next(err));
+          } else {
+            err = new Error(`Post ${req.params.postId} not found`);
+            err.status = 404;
+            return next(err);
+          }
+        })
+        .catch((err) => next(err));
+    }
+  );
+
+postRouter
+  .route("/:postId/comments/:commentId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .populate("comments.commentCreator")
+      .then((post) => {
+        const comment = post.comments.id(req.params.commentId);
+        if (post && comment) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(post.comments.id(req.params.commentId));
+        } else if (!post) {
+          const err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          const err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end(
+      `POST operation not supported on /posts/${req.params.postId}/comments/${req.params.commentId}`
+    );
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        const comment = post.comments.id(req.params.commentId);
+        if (post && comment) {
+          //console.log(comment.commentCreator._id.equals(req.user._id));
+          if (comment.commentCreator._id.equals(req.user._id)) {
+            if (req.body.commentContent) {
+              comment.commentContent = req.body.commentContent;
+            }
+            post
+              .save()
+              .then((post) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(post);
+              })
+              .catch((err) => next(err));
+          } else {
+            const err = new Error(`You are not the comment creator`);
+            err.status = 404;
+            return next(err);
+          }
+        } else if (!post) {
+          const err = new Error(`Post ${req.params.postId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          const err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Post.findById(req.params.postId)
+      .then((post) => {
+        const comment = post.comments.id(req.params.commentId);
+        if (post && comment) {
+          /*console.log(req.user._id);
+          console.log(comment.author._id);
+          console.log(comment.author._id.equals(req.user._id));*/
+          if (comment.commentCreator._id.equals(req.user._id)) {
+            //console.log("removing comment");
+            comment.remove();
+            post
+              .save()
+              .then((post) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(post);
+              })
+              .catch((err) => next(err));
+          } else {
+            const err = new Error(`You are not the comment creator`);
+            err.status = 404;
+            return next(err);
+          }
+        } else if (!post) {
+          err = new Error(`Post ${req.params.campsiteId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
+
+module.exports = postRouter;
